dae9d74cf424c5ede91357b2cd69bfb6505219ad
jacobTraugott <jacob.r.traugott@gmail.com>
2020-10-13 00:51:24 -0500

    Project Initialization

---
+++ MovieMatch/MovieMatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieMatch
+//
+//  Created by Jacob Traugott on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieMatch/MovieMatch/MovieMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatchApp.swift
+//  MovieMatch
+//
+//  Created by Jacob Traugott on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
